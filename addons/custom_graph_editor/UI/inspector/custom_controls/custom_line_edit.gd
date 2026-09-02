ddc188711fb4665a7c815aa5c40431e33ab75958
Tehelka <17590418+tehelka-gamedev@users.noreply.github.com>
2026-09-02 21:00:53 +0200

    Fix error spamming in custom_line_edit by properly building the popup menu

--- addons/custom_graph_editor/UI/inspector/custom_controls/custom_line_edit.gd
+++ addons/custom_graph_editor/UI/inspector/custom_controls/custom_line_edit.gd
@@ -9,32 +9,51 @@ signal text_updated(new_text: String)
 
 var _last_text: String = ""
 
+
 func _ready():
     context_menu_enabled = true
     # shortcut_keys_enabled = false # cannot delete just some of them...
 
-    var menu : PopupMenu = get_menu()
-
-    menu.item_count = 8
-
-    menu.remove_item(menu.get_item_index(MENU_EMOJI_AND_SYMBOL))
-    # remove separators
-    menu.remove_item(0) 
-    menu.remove_item(3)
+    # ontext menu is rebuilt every time it opens, so trim it on each popup
+    get_menu().about_to_popup.connect(_on_context_menu_about_to_popup)
 
     focus_exited.connect(_on_focus_exited)
     text_submitted.connect(_on_text_submitted)
 
+
 func set_text_no_signal(new_text: String):
     text = new_text
     _last_text = new_text
+
 
 func _on_focus_exited() -> void:
     if text != _last_text:
         _last_text = text
         self.text_updated.emit(text)
 
+
 func _on_text_submitted(new_text: String) -> void:
     if new_text != _last_text:
         _last_text = new_text
         self.text_updated.emit(new_text)
+
+
+func _on_context_menu_about_to_popup() -> void:
+    var menu: PopupMenu = get_menu()
+
+    var unwanted_ids: Array[int] = [
+        MENU_EMOJI_AND_SYMBOL,
+    ]
+    for id in unwanted_ids:
+        var idx: int = menu.get_item_index(id)
+        if idx != -1:
+            menu.remove_item(idx)
+
+    var i: int = menu.item_count - 1
+    while i >= 0:
+        if menu.is_item_separator(i):
+            var prev_is_sep: bool = i == 0 or menu.is_item_separator(i - 1)
+            var is_last: bool = i == menu.item_count - 1
+            if prev_is_sep or is_last:
+                menu.remove_item(i)
+        i -= 1
